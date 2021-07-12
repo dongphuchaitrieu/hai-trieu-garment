1ec0fc862e6ea08d7728ba8fd36db0b7b50a591d
dongphuchaitrieu <84892244+dongphuchaitrieu@users.noreply.github.com>
2021-07-12 11:44:10 +0700

    Made with love from Dong Phuc Hai Trieu

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} /> | Made with ♥️ from <a href="https://haitrieu.com/dong-phuc-hai-trieu">Đồng Phục Hải Triều</a>
         </a>
       </footer>
     </div>
